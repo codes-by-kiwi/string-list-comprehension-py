d870ce417a15d248d22bb600d99160c3c58ad19c
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-01 13:05:21 +0200

    Add files via upload

---
+++ string-listcomp.py
@@ -0,0 +1,4 @@
+examplelist = ("Anne", "Cuthbert", 12)
+example_string = "Hello, this is %s %s. Her age is %s." # %s can be used to access list elements 
+
+print(example_string % examplelist) #the "%s" in strings is replaced by the examplelist elements 
